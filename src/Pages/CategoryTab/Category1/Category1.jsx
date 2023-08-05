123f2c38e5e15aa92beaa19e24846ee6c4082699
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-08-05 12:59:46 +0600

    category tab updated

--- src/Pages/CategoryTab/Category1/Category1.jsx
+++ src/Pages/CategoryTab/Category1/Category1.jsx
@@ -1,42 +1,48 @@
-import React, { useContext } from 'react';
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer, toast } from 'react-toastify';
-import { Link, useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../Provider/AuthProvider';
+import React, { useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
-const Category1 = ({subCate}) => {
-  const {user} = useContext(AuthContext)
-    const navigate = useNavigate()
-    const {  picture, price, rating, toyName,_id} = subCate;
-    const handleDetails = () =>{
-        if(!user){
-          toast("You need to log in first")
-          setTimeout(() => {
-            navigate('/login')
-          }, 3000);
-         }
-        
+const Category1 = ({ subCate }) => {
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const { picture, price, rating, toyName, _id } = subCate;
+  const handleDetails = () => {
+    if (!user) {
+      toast("You need to log in first");
+      setTimeout(() => {
+        navigate("/login");
+      }, 3000);
     }
-    return (
-        <div className="card w-96 bg-base-100 shadow-xl border-2 p-4">
-  <figure><img className='border-2 rounded-lg' src={picture} alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      {toyName}
-      
-    </h2>
-    <div className="badge p-3 ">Price: ${price}</div>
-      <div className="badge p-3 badge-outline bg-yellow-500 border-yellow-500">Rating: {rating}</div>
-    
-    <div className="card-actions justify-end">
-    <Link to={`/singleToy/${_id}`}>
-      <button onClick={handleDetails} className='btn bg-blue-500'>View Details</button>
-      </Link>
+  };
+  return (
+    <div className="card w-96 shadow-xl border-2 hover:bg-fuchsia-200">
+      <figure>
+        <img
+          className="border-2 rounded-lg h-[250px] w-full object-cover"
+          src={picture}
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body bg-fuchsia-100  hover:bg-fuchsia-200">
+        <h2 className="card-title">{toyName}</h2>
+        <div className="badge ">Price: ${price}</div>
+        <div className="badge  badge-outline bg-yellow-500 border-yellow-500">
+          Rating: {rating}
+        </div>
+
+        <div className="card-actions justify-end">
+          <Link to={`/singleToy/${_id}`}>
+            <button onClick={handleDetails} className="btn bg-blue-500">
+              View Details
+            </button>
+          </Link>
+        </div>
+      </div>
+      <ToastContainer></ToastContainer>
     </div>
-  </div>
-  <ToastContainer></ToastContainer>
-</div>
-    );
+  );
 };
 
 export default Category1;
--- src/Pages/CategoryTab/CategoryTab.jsx
+++ src/Pages/CategoryTab/CategoryTab.jsx
@@ -1,14 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import React, { useEffect, useState } from "react";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import Category1 from "./Category1/Category1";
 import Category2 from "./Category2/Category2";
 import Category3 from "./Category3/Category3";
 
-
 const CategoryTab = () => {
-
-  
   const [active, setActive] = useState(0);
   const [subCategoryOne, setSubCategoryOne] = useState([]);
   const [subCategoryTwo, setSubCategoryTwo] = useState([]);
@@ -18,19 +15,23 @@ const CategoryTab = () => {
     fetch(`https://disney-dolls-server-snowy.vercel.app/addToy`)
       .then((res) => res.json())
       .then((data) => {
-        const subCategory1 = data.filter(item => item.category === "Frozen Dolls");
-        const subCategory2 = data.filter((item) => item.category === "Disney Princess Dolls");
-        const subCategory3 = data.filter((item) => item.category === "Villain Dolls");
+        const subCategory1 = data.filter(
+          (item) => item.category === "Frozen Dolls"
+        );
+        const subCategory2 = data.filter(
+          (item) => item.category === "Disney Princess Dolls"
+        );
+        const subCategory3 = data.filter(
+          (item) => item.category === "Villain Dolls"
+        );
         setSubCategoryOne(subCategory1);
         setSubCategoryTwo(subCategory2);
         setSubCategoryThree(subCategory3);
-        
       });
   }, []);
 
   const handleSubCategory = (index) => {
     setActive(index);
-
   };
 
   return (
@@ -38,39 +39,38 @@ const CategoryTab = () => {
       <h3 className="text-blue-700 text-4xl text-center my-10 font-bold ">
         Shop By Category
       </h3>
-      <div className="lg:mx-52 mb-10">
-
-
+      <div className="lg:mx-20 mb-10">
         <Tabs selectedIndex={active} onSelect={handleSubCategory}>
           <TabList className="my-3 lg:mx-20">
-            
-            <Tab className="btn mb-6 w-64 lg:w-[200px] btn-primary mr-2">Frozen Dolls</Tab>
-            <Tab className="btn mb-6 w-64 btn-primary mr-2">Disney Princess Dolls</Tab>
+            <Tab className="btn mb-6 w-64 lg:w-[200px] btn-primary mr-2">
+              Frozen Dolls
+            </Tab>
+            <Tab className="btn mb-6 w-64 btn-primary mr-2">
+              Disney Princess Dolls
+            </Tab>
             <Tab className="btn w-64 btn-primary">Villain Dolls</Tab>
           </TabList>
 
           <TabPanel>
-            
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            {
-              subCategoryOne.map(subCate => <Category2 key={subCate._id} subCate={subCate}></Category2>)
-            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
+              {subCategoryOne.map((subCate) => (
+                <Category2 key={subCate._id} subCate={subCate}></Category2>
+              ))}
             </div>
           </TabPanel>
-          
 
           <TabPanel>
-          <div className="grid grid-cols-1  lg:grid-cols-2 gap-6">
-            {
-              subCategoryTwo.map(subCate => <Category1 key={subCate._id} subCate={subCate}></Category1>)
-            }
+            <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {subCategoryTwo.map((subCate) => (
+                <Category1 key={subCate._id} subCate={subCate}></Category1>
+              ))}
             </div>
           </TabPanel>
           <TabPanel>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-            {
-              subCategoryThree.map(subCate => <Category3 key={subCate._id} subCate={subCate}></Category3>)
-            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {subCategoryThree.map((subCate) => (
+                <Category3 key={subCate._id} subCate={subCate}></Category3>
+              ))}
             </div>
           </TabPanel>
         </Tabs>
